3f7c2256cc70a2839ea5eab0408ab53589f4f640
Leejoongjae123 <ljj3347@naver.com>
2024-12-16 15:30:05 +0900

    '123'

--- app/(main-pages)/layout.js
+++ app/(main-pages)/layout.js
@@ -31,8 +31,6 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // app 디렉토리 사용 여부 확인
+  }
+};
 
 module.exports = nextConfig;
